46bd94e88ad0bad9fe6478d5ab33b5f11e1d57d3
Tri Le <letrin93@gmail.com>
2022-09-29 14:02:42 -0600

    save

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 import { baseURL } from '../env'
-export const SandboxApi = Axios.create({
-  baseURL: "https://bcw-sandbox.herokuapp.com",
+export const api = Axios.create({
+  baseURL,
   timeout: 8000
 })
